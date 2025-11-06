6c4120bc2fe6ba1fc204530bb94779fe7c3808ea
evelynokner <oknerevelyn@gmail.com>
2025-11-05 20:28:45 -0500

    completed flood fill code

--- minesweeper/floodfill.py
+++ minesweeper/floodfill.py
@@ -17,12 +17,12 @@ def dfs(gameboard, count_matrix, x, y):
 
     # if [x][y] is opened, do nothing
     # if [x][y] == 1 
-    if(gameboard[x][y] == openedCell):
+    '''
+    if gameboard[x][y] == openedCell:
         print("bye")
-        return
-    # if cell isn't open, mark as open
-    #if(gameboard[x][y] == closedCell):
-    else:
+        return'''
+    # if cell isn't open, mark as open (this will only happen with the first coordinate
+    if gameboard[x][y] == closedCell:
         print("marked as open")
         gameboard[x][y] = openedCell
 
@@ -31,13 +31,18 @@ def dfs(gameboard, count_matrix, x, y):
     print(f"neighbours: {neighbours}")
     for (nx, ny) in neighbours:
         # open neighbour cells
-        print(f'opening cell: {nx} {ny}')
-        gameboard[nx][ny] = openedCell
+        #print(f'opening cell: {nx} {ny}')
+        #gameboard[nx][ny] = openedCell
         # check count
         # if neighbour has no adjacent mines, perform dfs (open cell & adjacent cells)
-        # that means that the value at neighbour coordinates is 0 in count_matrix 
+        # that means that the value at neighbour coordinates is 0 in count_matrix
+        # if neighbor is already open, do nothing
+        if gameboard[nx][ny] == openedCell:
+            continue
+
         if count_matrix[nx][ny] == 0:
             print("count is 0")
+            gameboard[nx][ny] = openedCell
             dfs(gameboard, count_matrix, nx, ny)
 
 # test method for changing values in an array
@@ -49,24 +54,17 @@ def test (arr1, arr2):
     arr2[2] = 70
     print(arr1, arr2)
 
-'''
+
 if __name__ == "__main__":
     # Input initialization
-    matrix = [[1, 1, 1, 0],
-              [0, 1, 1, 1],
-              [1, 0, 1, 1]]
-    
-    # initial matrix
-    for row in matrix:
-        print(" ".join(map(str, row)))
+    gameboard = [[0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]]
 
-    print("\n")
-    sr, sc, newColor = 1, 2, 2
-
-    # Perform flood fill
-    result = floodFill(matrix, sr, sc, newColor)
-
-    # output grid
-    for row in result:
-        print(" ".join(map(str, row)))
-'''
+    count_matrix = [[0, 1, -1, 1],
+                    [0, 1, 1, 1],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0]]
+    # call dfs
+    result = dfs(gameboard, count_matrix, 1, 0)
