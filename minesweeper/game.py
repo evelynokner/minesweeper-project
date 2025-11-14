04a4ca70034cbf352d56049dc74775590683974a
evelynokner <oknerevelyn@gmail.com>
2025-11-14 15:40:16 -0500

    created module for constants

---
+++ minesweeper/game.py
@@ -0,0 +1,8 @@
+"""
+def initialize_board(num_rows, num_cols)
+        def move -> call dfs
+        check if game is over
+            - 3 states of game: continue, won, lost
+            if game over, return indicator of result (win or loss)
+            otherwise, return indicator of "game continues"
+"""
